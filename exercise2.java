f90bae589edb2e5b61fade6177893637ce395b8f
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-11-27 20:16:31 +0800

    Create exercise2.java

---
+++ exercise2.java
@@ -0,0 +1,27 @@
+package lecClass;
+
+public class exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int k = 20;
+		int j = 20;
+		int i;
+		while (k>10) {
+			System.out.println("K = "+k);
+			k--;
+		}
+		
+		do {
+			System.out.println("j = "+j);
+			j--;
+		}
+		while(j>10);
+			
+		for(i=20; i>10; i--) {
+			System.out.println("i = "+i);
+		}
+	}
+
+}
